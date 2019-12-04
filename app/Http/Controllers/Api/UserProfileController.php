0ac2723fae972c256696cb0af2bbbc77e36cde9e
wahid09 <wahid.ullah.cse@gmail.com>
2019-12-04 22:25:46 +0600

    Passport authentication create

--- app/Http/Controllers/Api/UserProfileController.php
+++ app/Http/Controllers/Api/UserProfileController.php
@@ -10,6 +10,9 @@ use Illuminate\Http\Request;
 
 class UserProfileController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *
